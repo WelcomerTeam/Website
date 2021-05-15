97b8245c22effc35d7a868cb9cdebc3c58faee04
blane thompson <blane1245@hotmail.co.uk>
2021-05-15 21:58:00 +0100

    Added beginnings of index page

---
+++ app/tailwind.config.js
@@ -0,0 +1,37 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Rubik', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'primary': {
+          light: '#448DEF',
+          DEFAULT: '#2F80ED',
+          dark: '#2A73D5',
+        },
+        'secondary': {
+          light: '#333333',
+          DEFAULT: '#202225',
+          dark: '#1C1E21',
+        },
+        'donate': {
+          light: '#E088D7',
+          DEFAULT: '#D96BCE',
+          dark: '#AD55A4',
+        },
+        'dace': '#72DACE',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
